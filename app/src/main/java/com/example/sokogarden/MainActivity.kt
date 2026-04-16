237e0031f636f8dd5828f3a5ac312e32905c95ab
telvinnjuguna17-ops <telvinnjuguna17@gmail.com>
2026-04-16 12:14:09 +0300

    new work

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.sokogarden
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +36,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, Signin::class.java)
             startActivity(intent)
         }
+//        Find the recyclerview and progressbar by use of their ids
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val progressBar = findViewById< ProgressBar>(R.id.progressbar)
+
+//        specify the api endpoint for fetching data (alwaysdata)
+        val url = "https://telvin.alwaysdata.net/api/get_products"
+
+//        Import the helper class
+        val helper = ApiHelper(applicationContext)
+
+//        Inside of the helper class access the function loadproducts
+        helper.loadProducts(url,recyclerView,progressBar)
     }
 
 }
--- app/src/main/java/com/example/sokogarden/Signup.kt
+++ app/src/main/java/com/example/sokogarden/Signup.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,46 @@ class Signup : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Find all views by use of their ids
+        val username = findViewById<EditText>(R.id.username)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val phone = findViewById<EditText>(R.id.phone)
+        val signupButton = findViewById<Button>(R.id.signupBtn)
+        val signinTextView = findViewById<TextView>(R.id.signintxt)
+
+        //Below when a person clicks on the textview they are navigated to the signin page
+        signinTextView.setOnClickListener {
+            val intent = Intent(applicationContext, Signin::class.java)
+            startActivity(intent)
+        }
+
+        //On click of the signup button,we want to register a person
+        signupButton.setOnClickListener {
+            //Specify the Api endpoints
+            val api = "https://kbenkamotho.alwaysdata.net/api/signup"
+
+            //Create a RequestParams-it is where we are going to hold all data
+            val data = RequestParams()
+
+            // Add/Append the username,email,password and phone on the data
+            data.put("username", username.text.toString().trim())
+            data.put("email",email.text.toString().trim())
+            data.put("password",password.text.toString().trim())
+            data.put("phone", phone.text.toString().trim())
+
+            //Import the ApiHelper Class
+            val helper = ApiHelper(applicationContext)
+
+            // Inside of the helper class ,access the function post
+            helper.post(api,data)
+
+            //Clear the details
+            email.text.clear()
+            password.text.clear()
+            phone.text.clear()
+            username.text.clear()
+        }
+        }
+
     }
-}
