e72dba764fa3efa0294bfa1a31fa79f32185f8f6
telvinnjuguna17-ops <telvinnjuguna17@gmail.com>
2026-04-20 11:33:31 +0300

    new work

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -48,6 +48,15 @@ class MainActivity : AppCompatActivity() {
 
 //        Inside of the helper class access the function loadproducts
         helper.loadProducts(url,recyclerView,progressBar)
+
+//        Find the about button by use of its id and have the intent
+        val aboutButton = findViewById<Button>(R.id.aboutBtn)
+
+//        Below is the intent of the About activity
+        aboutButton.setOnClickListener {
+            val intent = Intent(applicationContext, About::class.java)
+            startActivity(intent)
+        }
     }
 
 }
